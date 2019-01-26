a2ecba9b3e8e7533b1db1d6938d3a9d8ee0e7341
jdmiguel <jaimedmiguel@gmail.com>
2019-01-26 16:33:19 +0100

    defautProps added in Intro test

--- src/components/containers/Intro/test.js
+++ src/components/containers/Intro/test.js
@@ -7,6 +7,14 @@ import Intro from '.';
 import { findByTestAttr, checkProps } from '../../../utils/testUtils';
 import { intro } from '../../../utils/testLiterals';
 
+const defaultProps = { 
+    setUserData: PropTypes.func.isRequired,
+    setUserRepos: PropTypes.func.isRequired,
+    setUserFollowers: PropTypes.func.isRequired,
+    setUserFollowing: PropTypes.func.isRequired,
+    outIntro: PropTypes.func.isRequired
+}
+
 /**
  * Factory function to create a shallowWrapper for the Counter Component
  * @function setup
@@ -15,57 +23,34 @@ import { intro } from '../../../utils/testLiterals';
  */
 
 const setup = (props={}) => {
-    const wrapper = shallow(<Intro {...props}/>);
+    const setupProps = {...defaultProps, ...props}
+    const wrapper = shallow(<Intro {...setupProps}/>);
     return wrapper;
 }
 
 test('renders div container without error', () => {
-    const wrapper = setup({ 
-        setUserData: PropTypes.func.isRequired,
-        setUserRepos: PropTypes.func.isRequired,
-        setUserFollowers: PropTypes.func.isRequired,
-        setUserFollowing: PropTypes.func.isRequired,
-        outIntro: PropTypes.func.isRequired
-    });
+    const wrapper = setup();
     const counterComponent = findByTestAttr(wrapper, intro.container);
 
     expect(counterComponent.length).toBe(1);
 });
 
 test('renders githubCorner component without error', () => {
-    const wrapper = setup({ 
-        setUserData: PropTypes.func.isRequired,
-        setUserRepos: PropTypes.func.isRequired,
-        setUserFollowers: PropTypes.func.isRequired,
-        setUserFollowing: PropTypes.func.isRequired,
-        outIntro: PropTypes.func.isRequired
-    });
+    const wrapper = setup();
     const counterComponent = findByTestAttr(wrapper,intro.githubCorner);
 
     expect(counterComponent.length).toBe(1);
 });
 
 test('renders Header component without error', () => {
-    const wrapper = setup({ 
-        setUserData: PropTypes.func.isRequired,
-        setUserRepos: PropTypes.func.isRequired,
-        setUserFollowers: PropTypes.func.isRequired,
-        setUserFollowing: PropTypes.func.isRequired,
-        outIntro: PropTypes.func.isRequired
-    });
+    const wrapper = setup();
     const counterComponent = findByTestAttr(wrapper, intro.header);
 
     expect(counterComponent.length).toBe(1);
 });
 
 test('renders Input component without error', () => {
-    const wrapper = setup({ 
-        setUserData: PropTypes.func.isRequired,
-        setUserRepos: PropTypes.func.isRequired,
-        setUserFollowers: PropTypes.func.isRequired,
-        setUserFollowing: PropTypes.func.isRequired,
-        outIntro: PropTypes.func.isRequired
-    });
+    const wrapper = setup();
     const counterComponent = findByTestAttr(wrapper, intro.input);
 
     expect(counterComponent.length).toBe(1);
