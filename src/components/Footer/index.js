f4f464a2f7ebb95a875cce9bf89521e975ee14e4
jdmiguel <jaimedmiguel@gmail.com>
2019-10-30 15:59:58 +0100

    fixed footer styles

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,14 +5,13 @@ const Footer = () => {
   return (
     <div className="footer">
       <h3>
-        Created by
+        Created by{' '}
         <a
           href="https://jdmiguel.com"
           target="_blank"
           rel="noopener noreferrer"
           aria-label="View source on GitHub"
         >
-          {' '}
           jdmiguel
         </a>
       </h3>
