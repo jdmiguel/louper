ddc1a46abc2b006d8564df20d1174159a5c05924
jdmiguel <jaimedmiguel@gmail.com>
2022-09-25 09:41:56 +0100

    update packages and eslint rules

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
-import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import initMockServiceWorker from './mocks/mockServiceWorker';
 import App from './components/App';
 
 initMockServiceWorker();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
